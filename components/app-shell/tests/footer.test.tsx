10ef85567d283568bc01c919d6deb9958d227b14
James Walsh <jamesandersonwalsh@gmail.com>
2025-05-21 19:55:53 -0600

    test: 💍 update footer.test.tsx

--- components/app-shell/tests/footer.test.tsx
+++ components/app-shell/tests/footer.test.tsx
@@ -25,6 +25,6 @@ describe('components/app-shell/footer', () => {
   it('displays copyright timestamp', () => {
     render(<Footer />)
 
-    expect(screen.getByText(/© 2024 James Walsh/i)).toBeInTheDocument()
+    expect(screen.getByText(/© 2025 James Walsh/i)).toBeInTheDocument()
   })
 })
