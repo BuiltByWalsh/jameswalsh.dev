4e89c6cf1edc05fde93c91515c39edd1fc989fea
James Walsh <jamesandersonwalsh@gmail.com>
2025-07-03 20:43:50 -0600

    fix: 🐛 mode toggle button positioning

--- components/app-shell/action-buttons.tsx
+++ components/app-shell/action-buttons.tsx
@@ -19,8 +19,8 @@ export function ModeToggleButton() {
       <Tooltip delayDuration={250}>
         <TooltipTrigger asChild>
           <Button variant="ghost" onClick={handleToggleTheme} className="size-5 !p-5">
-            <Moon className="fixed size-5 scale-100 dark:scale-0" />
-            <Sun className="fixed size-5 scale-0 dark:scale-100" />
+            <Moon className="absolute size-5 scale-100 dark:scale-0" />
+            <Sun className="absolute size-5 scale-0 dark:scale-100" />
           </Button>
         </TooltipTrigger>
         <TooltipContent side="bottom">
